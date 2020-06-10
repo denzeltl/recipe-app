124625e512b0ae4cf26681d0de2b3a08ed157e92
Denzel <denzeltlee@gmail.com>
2020-06-10 18:15:07 +0800

    Updates

---
+++ precache-manifest.0b30038d63b363df4b8b755e3d0a2cf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "374553cabbde418bec97926c06d36449",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "64599b450c77ebe2c941",
+    "url": "/recipe-app/static/css/main.6411b5ea.chunk.css"
+  },
+  {
+    "revision": "0a1dc217f4ef93467872",
+    "url": "/recipe-app/static/js/2.9896821f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-app/static/js/2.9896821f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64599b450c77ebe2c941",
+    "url": "/recipe-app/static/js/main.af282e88.chunk.js"
+  },
+  {
+    "revision": "7ff5b27d3f9efd2f4352",
+    "url": "/recipe-app/static/js/runtime-main.96283c75.js"
+  }
+]);
